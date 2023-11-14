fe8488f079f27bb173502adf56052f358c358758
Maxim <nevratov22@gmail.com>
2023-11-14 13:00:26 +0300

    data layer has been completed

--- app/src/main/java/com/example/shoppinglist/domain/ShopListRepository.kt
+++ app/src/main/java/com/example/shoppinglist/domain/ShopListRepository.kt
@@ -3,7 +3,7 @@ package com.example.shoppinglist.domain
 interface ShopListRepository {
     fun addShopItem(shopItem: ShopItem)
 
-    fun deleteShopItem(shopItemId: Int)
+    fun deleteShopItem(shopItem: ShopItem)
 
     fun editShopItem(shopItem : ShopItem)
 
